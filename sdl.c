56f80e1e50da7c4b0081ec18b833dca5871307ae
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-07-13 23:22:26 +0100

    Add files via upload

--- sdl.c
+++ sdl.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
     // Update the texture with the pixel data
     clears(0,255,255);
     char *c="hello sdl....";
-    gputsx4l(300,300,0,0,0,c);
+    gputsx4c(300,300,0,0,0,c);
 
     
      
